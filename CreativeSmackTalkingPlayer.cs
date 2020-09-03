5a2542019639a3d3bc20e8d3fc301532bc06134c
MandasK <king.amanda.r@gmail.com>
2020-09-03 13:14:43 -0500

    Code review 1st change

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -6,17 +6,16 @@ namespace ShootingDice
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
     public class CreativeSmackTalkingPlayer : Player
     {
-
+        private List<string> tauntList = new List<string>()
+        {
+            "You will never beat me!",
+            "I'm the best ever!",
+            "High Roller in the house! It's me. I'm hte high roller",
+            "BOOOOOOOOOOOO. You're gonna lose!",
+            "You can't even count to 6 much less roll one!"
+        };
         public override void Play(Player other)
         {
-            List<string> tauntList = new List<string>()
-            {
-                "You will never beat me!",
-                "I'm the best ever!",
-                "High Roller in the house! It's me. I'm hte high roller",
-                "BOOOOOOOOOOOO. You're gonna lose!",
-                "You can't even count to 6 much less roll one!"
-            };
 
             Random rNum = new Random();
             int numIndex = rNum.Next(tauntList.Count);
